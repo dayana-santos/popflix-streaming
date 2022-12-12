de0fb7e000e4e46b3e362bb2a6c9ff43cde885fb
DESKTOP-CUSN0GU\dyana <ux.dayanasantos@gmail.com>
2022-12-12 18:04:08 -0300

    lista de filmes dinâmica, utilizando API

---
+++ public/js/api.js
@@ -0,0 +1,13 @@
+
+/*chave da API*/
+let api_key = "1ffa7ba9ad8ccf01e802e79c1a42e3e9";
+
+/*link para importar as imagens*/
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+/*link para importar os gêneros de filme*/
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+/*link para descoberta dos filmes*/
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
